98c78f4657de1a87d85f14426030273b6e2199a1
Toshiharu Izumi <67109268+toizumi03@users.noreply.github.com>
2024-04-28 13:30:46 +0900

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,54 @@
+#winget search *
+
+#PowerShell core
+winget install Microsoft.PowerShell
+pwsh.exe
+
+#VS Code
+winget install Microsoft.VisualStudioCode
+
+#Storage Explorer
+winget install Microsoft.Azure.StorageExplorer
+
+#Bicep
+winget install Microsoft.Bicep
+
+#Azure PowerShell
+Install-Module Az
+
+#Azure CLI
+winget install Microsoft.AzureCLI
+
+#Sysinternal suite
+winget install 9P7KNL5RWT25
+
+#Google Chrome
+winget install Google.Chrome
+
+#Git
+winget install Git.Git
+winget install GitHub.cli
+
+#WSL
+wsl --install -d Ubuntu-20.04
+
+#PowerToys
+winget install Microsoft.PowerToys
+
+#DevToys
+winget install 9PGCV4V3BK4W
+
+#Mouse without borders
+Microsoft.MouseWithoutBorders
+
+#Wireshark
+winget install WiresharkFoundation.Wireshark
+
+#Stream Deck
+winget install Elgato.StreamDeck
+
+#Beef Text
+winget install Beeftext.Beeftext
+
+#Logitech.Options
+winget install Logitech.Options
